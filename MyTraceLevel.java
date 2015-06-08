570b942f846e245fa9dc2be8049a2be15c8d9063
GurpreetKaur7 <er.gurpreetkaur7@gmail.com>
2015-06-09 02:22:29 +0530

    Create MyTraceLevel.java

---
+++ MyTraceLevel.java
@@ -0,0 +1,26 @@
+
+
+import org.apache.log4j.Level;
+
+public class MyTraceLevel extends Level { 
+            
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final int APP_INFO_INT = 19000; 
+	public static final int APP_DEBUG_INT= 18000; 
+	public static final int APP_TRACE_INT = 17000; 
+	public static final Level APP_INFO = new MyTraceLevel(APP_INFO_INT,"APP_INFO",7);
+	public static final Level APP_DEBUG = new MyTraceLevel(APP_DEBUG_INT,"APP_DEBUG",8);
+	public static final Level APP_TRACE = new MyTraceLevel(APP_TRACE_INT,"APP_TRACE",9);
+	 
+ 
+
+ protected MyTraceLevel(int arg0, String arg1, int arg2) {
+         super(arg0, arg1, arg2);
+
+     }
+
+}
